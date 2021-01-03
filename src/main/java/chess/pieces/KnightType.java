dc1bab4c3cca84092365eba84df9ee37e8fee4a8
lukas <mr.halleg@gmail.com>
2021-01-03 21:52:21 +0100

    reworked PieceType to only gnerate moves from a PieceData

--- src/main/java/chess/pieces/KnightType.java
+++ src/main/java/chess/pieces/KnightType.java
@@ -2,17 +2,18 @@ package chess.pieces;
 
 import chess.Party;
 import chess.Position;
+import chess.board.PieceData;
 import chess.moves.Move;
 
 import java.util.List;
 
 public class KnightType extends PieceType{
     public KnightType(Party party, Position pos) {
-        super(party, pos);
+        super();
     }
 
     @Override
-    public List<Move> generatePossibleMoves() {
+    public List<Move> generatePossibleMoves(PieceData data) {
         return null;
     }
 }
